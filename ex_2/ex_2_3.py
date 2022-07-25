3597f7043454ae55130238581b9ae4333add4c5d
Marianna Chantzi <109886683+MariannaChantzi@users.noreply.github.com>
2022-07-25 11:15:59 +0300

    Delete ex_2_3.py

--- ex_2/ex_2_3.py
+++
@@ -1,6 +0,0 @@
-#Prompt the user for hours and rate per hour to compute gros pay
-
-hours = input("Enter Hours: ")
-rate = input("Enter Rate per Hour: ")
-pay = float(hours) * float(rate)
-print("Pay:",pay)
